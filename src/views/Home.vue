99b60addcea278850f4faf8115450c8d7be15832
Abraham Peter <theclassical001@gmail.com>
2022-01-22 18:41:37 +0100

    pr-v home, datatitle, and moment implemented

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="home text-center">
-    
-  </div>
+  <main v-if="!loading">
+      <DataTitle :text="title" :dataDate="dataDate" />
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center items-center">
+      <div class="text-gray-500 text-3xl mt-10 mb-6">Fatching</div>
+      <img :src="loadingImage" class="w-24 m-auto">
+  </main>
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
+
 export default {
-  name: 'Home',
-  props: {
-    msg: String
-  }
+    name: 'Home',
+    components:{
+        DataTitle
+    },
+    data() {
+        return {
+            loading: true,
+            title: 'Global',
+            dataDate: '',
+            status:{},
+            countries: [],
+            loadingImage: require('../assets/01.gif'),
+        }
+    },
+    methods: {
+        async fetchData() {
+            const res = await fetch('https://api.covid19api.com/summary')
+            const data = await res.json()
+            return data
+        },
+    },
+    async created() {
+        const data = await this.fetchData()
+
+        this.dataDate = data.Date
+        this.status = data.Global
+        this.countries = data.Countries
+        this.loading  = false
+    }
 }
 </script>
