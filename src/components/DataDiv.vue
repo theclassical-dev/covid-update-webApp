03994d93b27751290f4764589723f6aaf9bfdabc
Abraham Peter <theclassical001@gmail.com>
2022-01-22 20:40:11 +0100

    pr-v recovery fetch implemented

--- src/components/DataDiv.vue
+++ src/components/DataDiv.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="grid md:grid-cols-2 gap-4">
+    <div class="grid md:grid-cols-3 gap-4">
         <!-- #1 div -->
         <div class="shadow-md bg-blue-100 p-10 text-center rounded">
             <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
@@ -26,8 +26,22 @@
                 {{ numWithCommas(status.TotalDeaths) }}
             </div>
         </div>
+
+        <!-- #1 div -->
+        <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Recovery</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ numWithCommas(status.NewRecovered) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ numWithCommas(status.TotalRecovered) }}
+            </div>
+        </div>
+        
     </div>
-    
+
 </template>
 
 <script>
