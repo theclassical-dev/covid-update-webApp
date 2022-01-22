7e056525a853275dcc88ed4ad024022bc1c20e8a
Abraham Peter <theclassical001@gmail.com>
2022-01-22 15:59:19 +0100

    pr-v header implemented

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header>
+		<div class="text-center bg-yellow-500 text-white p-4 mb-4">
+			<h1 class="text-3xl md:text-5xl uppercase font-semibold mb-3">
+				<i class="fa fa-viruses text-3xl"></i>
+				Covid-19 Update
+			</h1>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home text-center">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
